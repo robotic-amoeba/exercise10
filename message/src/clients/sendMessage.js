f1a2fd999ce7c727ccab4336db84f538140ef267
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-11-14 19:12:20 +0100

    Some cleaning and improvement in the logic around the circuit

--- message/src/clients/sendMessage.js
+++ message/src/clients/sendMessage.js
@@ -6,77 +6,46 @@ const messageAPP = axios.create({
   baseURL: "http://localhost:3000",
   timeout: 2000
 });
-const rollBackPolicy = require("../controllers/rollbackQueue");
 
-const circuitBreaker = require("opossum");
 const options = {
   timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
   errorThresholdPercentage: 30, // When 50% of requests fail, trip the circuit
   resetTimeout: 3000 // After 30 seconds, try again.
 };
+const circuitBreaker = require("opossum");
 const circuit = circuitBreaker(requestToMessageAPP, options);
-circuit.on("timeout", a => console.log("TIMEOUT: timeout in the circuit", a));
-circuit.on("reject", () => {
-  console.log(`REJECT: The circuit is open. Failing fast.`);
-});
-circuit.on("halfOpen", a => console.log(`CIRCUIT HALF_OPEN after ${a}ms of recovery time`));
-circuit.on("close", () => console.log("CIRCUIT CLOSED"));
-circuit.on("open", () => console.log("CIRCUIT OPENED"));
 
-function requestToMessageAPP(message, retries) {
+function requestToMessageAPP(message) {
   return messageAPP
     .post("/message", message)
     .then(response => {
       debug("Success sending the message: Response: ", response.data);
       message.status = "OK";
-      saveMessage(message);
       return message;
     })
     .catch(error => {
-      let customError;
       if (error.response || error.request) {
         debug("Error in messageapp");
         message.status = "ERROR";
-
         if (error.code && error.code === "ECONNABORTED") {
           debug("Timeout Exceeded!");
           message.status = "TIMEOUT";
-          saveMessage(message);
           throw error.code;
         }
-
-        saveMessage(message);
-        retries--;
-        //retryPolicy(message, retries);
       } else {
         debug("Error in HTTP request");
         message.status = "ERROR";
-        saveMessage(message);
-        retries--;
-        //retryPolicy(message, retries);
       }
-      debug("retries left: ", retries);
       throw "ERROR";
     });
 }
 
-function retryPolicy(message, retries) {
-  let fatalErrorsCount = 0;
-  if (retries > 0) {
-    //timeout increases with every retry up to 15s
-    debug(`Messageapp communication failed: retrying in ${Math.floor(15000 / retries)} seconds`);
-    setTimeout(() => {
-      requestToMessageAPP(message, retries);
-    }, Math.floor(15000 / retries));
-  } else {
-    debug("Fatal error after 5 retries. Returning cash to account");
-    fatalErrorsCount++;
-    rollBackPolicy(message);
-    if (fatalErrorsCount === 10) {
-      console.log("10 fatal errors occurred. Could be nothing, but check Messageapp");
-    }
-  }
-}
+circuit.on("timeout", a => console.log("TIMEOUT: timeout in the circuit", a));
+circuit.on("reject", () => {
+  console.log(`REJECT: The circuit is open. Failing fast.`);
+});
+circuit.on("close", () => console.log("CIRCUIT CLOSED"));
+circuit.on("open", () => console.log("CIRCUIT OPENED"));
 
 module.exports = function(messageReq) {
   const message = {
@@ -84,12 +53,22 @@ module.exports = function(messageReq) {
     body: messageReq.body
   };
   if (messageReq.status === "PAYED") {
-    //requestToMessageAPP(message, 5);
     circuit
-      .fire(message, 5)
-      .then(a => console.log("continua el flujo de ejec:", a))
-      .catch(() => console.error("error en circuit-----------"));
-    circuit.fallback(a => console.log("fallback--------", a));
+      .fire(message)
+      .then(message => {
+        if (message) {
+          saveMessage(message);
+          console.log("saved in then: ", message);
+        }
+      })
+      .catch(e => console.error("Error inside the circuit: ", e));
+
+    circuit.fallback(message => {
+      if (message.status) {
+        saveMessage(message);
+        console.log("saved in fallback: ", message);
+      }
+    });
   } else {
     saveMessage(messageReq);
   }
